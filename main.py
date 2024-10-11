1dddd484b60c4b79e5049b3e3eb31231f70b9523
KushalRegmi61 <kushalbro82@gmail.com>
2024-10-11 11:50:13 +0545

    added  function for game functionality

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
 from tkinter import *
-
+import random
 
 window = Tk()
 
 
-# #creating a window
-# tk.title("Tic Tac Toe")
-# tk.geometry("400x400")
+#creating a x_state and o_state
+x_state = []   
+o_state = []
+
+#creating a turn variable
+turn = 0
 
 
-# #creating a label
-# lbl = Label(tk, text="Hello World")
-# lbl.grid(row=0, column=0)
-# #creating a input field
-# entry = Entry(tk)
-# entry.grid(row=1, column=0, columnspan=2)
-
-# #fucntion to get the input from the input field
-# def get_input():
-#     lbl.config(text=entry.get())
-
-# #creating a button
-# btn = Button(tk, text="Click Me", command=get_input)
-# btn.grid(row=2, column=0, columnspan=2)
-
-#language= canvas.create_text(400, 150, text="Language", font=("Times New Roman", 30, "bold"))
-
+#creating a winning_list
+winning_list = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 
 #creating a window
@@ -35,11 +23,24 @@ window.geometry("600x500")
 window.config(padx=100, pady=40)
 
 #creating a title_label
-title_label = Label(text="Start", font=("Times New Roman", 20, "bold"))
+title_label = Label(text="Let's Play", font=("Times New Roman", 20, "bold"))
 title_label.grid(row=0, column=1, columnspan=3)
+
+#creating a game_tied function  
+def game_tied():
+    global x_state, o_state
+    if x_state.count(1) + o_state.count(1) == 9: #count 1's in x_state and o_state
+        title_label.config(text="Game Tied")
+        for i in button_list:
+            i.config(command=DISABLED)
+
+
+#creating a button list
+button_list = []
 
 #creating cells for the game
 def create_cells():
+    global button_list
     for i in range(3):
         for j in range(3):
             cell = Button(
@@ -47,25 +48,96 @@ def create_cells():
                 font=("Times New Roman", 20, "bold"), 
                 width=8, 
                 height=3, 
-                # bg="lightblue", 
-                # fg="black", 
-                # relief="solid", 
-                borderwidth=1
+                borderwidth=2,
+                state=DISABLED,
             )
-            cell.grid(row=i+1, column=j+1, )
+            button_list.append(cell)
+            cell.grid(row=i+1, column=j+1)
 
-#creating two additional buttons 
+#function to print the board
+def printboard(x_state, o_state):     
+    #function to check game tied
+    game_tied()
+
+    #printing the board
+    for i in range(3):
+        for j in range(3):
+            if x_state[i*3+j] == 1:
+                button_list[i*3+j].config(text="X",  fg="red", command=DISABLED)
+            elif o_state[i*3+j] == 1:
+                button_list[i*3+j].config(text="O",  fg="blue", command=DISABLED)
+            
+            else:
+                button_list[i*3+j].config(text=" ")
+
+
+#checking the winner
+def check_winner(x_state, o_state):
+    for i in winning_list:
+        if x_state[i[0]] and x_state[i[1]] and x_state[i[2]]:
+            title_label.config(text="X wins")
+            printboard(x_state, o_state)
+
+            return 1
+        if o_state[i[0]] and o_state[i[1]] and o_state[i[2]]:
+            title_label.config(text="O wins")
+            printboard(x_state, o_state)
+            printboard(x_state, o_state)
+            return 0
+        
+    return -1
+
+#function to handle the button click
+def handle_click(row, col):
+    global turn, x_state, o_state
+    if turn:
+        title_label.config(text="O's turn")
+        x_state[row*3+col] = 1
+    else:
+        title_label.config(text="X's turn")
+        o_state[row*3+col] = 1
+    printboard(x_state, o_state)
+    result = check_winner(x_state, o_state)
+    if result == 1:
+        for i in button_list:
+            i.config(command=DISABLED)
+    elif result == 0:
+        for i in button_list:
+            i.config(command=DISABLED)
+    turn = 1 - turn
+
+
 # 1. start button
 start_button = Button(text="Start", font=("Times New Roman", 16, "bold"))
-start_button.grid(row=4, column=3, padx=15, pady=10)
+start_button.grid(row=4, column=2, padx=15, pady=10)
+
+#function to start the game
+def start_game():
+    global turn, x_state, o_state, button_list
+    turn = random.randint(0, 1)
+    x_state = [0,0,0,0,0,0,0,0,0]
+    o_state = [0,0,0,0,0,0,0,0,0]
+
+#enabling the buttons
+    for i in range(3):
+        for j in range(3):
+            button_list[i*3+j].config( state=NORMAL,command=lambda i=i, j=j: handle_click(i, j))
+
+    printboard(x_state, o_state)
+    title_label.config(text="0's turn" if   turn == 0 else "X's turn")
+
+#binding the start button to the start_game function
+    start_button.config(text="Restart")
+
+#binding the start button to the start_game function
+start_button.config(command=start_game)    
+     
 
 
-# 2. Restart button
-restart_button = Button(text="Restart", font=("Times New Roman", 16, "bold"))
-restart_button.grid(row=4, column=1, padx=10, pady=10)  
-restart_button.config(state=DISABLED)
-       
-
+# calling the create_cells function
 create_cells()
+
+
+
 #creating mainloop for the window
 window.mainloop()
